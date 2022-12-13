fde27215fcd3daf4c7ab1f5bb2bab159adec2d6b
Jacob Henrich <jhenrich@unomaha.edu>
2022-12-13 16:42:36 -0600

    Fixed Cart Path, added Cart button to search.

---
+++ src/util/Info.java
@@ -0,0 +1,9 @@
+package util;
+
+public interface Info {
+   public String projectName = "teamwebproject-Henrich";
+   public String searchWebNameAdmin = "simpleSearchAdmin.html";
+   public String searchWebName = "simpleSearchHB.html";
+   public String insertWebName = "simpleInsertHB.html";
+   public String LoginWebName = "accountPage.html";
+}
